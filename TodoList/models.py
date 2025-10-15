1edd1f6ae5fa133f0fa1db768f3f64fb43941a5f
Blessed <blessedmbang23@gmail.com>
2025-10-15 23:00:33 +0100

    Updated models and serializers with new field

--- TodoList/models.py
+++ TodoList/models.py
@@ -3,8 +3,10 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     completed = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField()
 
     @property
     def status(self):
--- TodoList/serializers.py
+++ TodoList/serializers.py
@@ -4,4 +4,4 @@ from . models import Task
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ['id', 'title', 'description', 'completed', 'status']
+        fields = '__all__'
